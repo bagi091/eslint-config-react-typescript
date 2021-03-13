7e5280937e035581e1e34d74ba8153b4135e7ee5
Aliaksandr Lakhvich <bagi091@gmail.com>
2021-03-13 19:25:21 +0300

    init

---
+++ index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+
+    // typescript-eslint specific options
+    warnOnUnsupportedTypeScriptVersion: true,
+  },
+  // parserOptions: {
+  //   // project: "./tsconfig.json",
+  // },
+
+  plugins: [
+    '@typescript-eslint',
+    // "prettier",
+  ],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'airbnb/whitespace',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    // "plugin:prettier/recommended",
+  ],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'react/prop-types': 'off',
+      },
+    },
+  ],
+  rules: {
+    //    "import/order": ['error', { groups: [['builtin', 'external', 'internal']] }]
+  },
+};
